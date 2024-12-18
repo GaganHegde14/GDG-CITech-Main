8ba3aff9ef09724924b7eba4c35886a7e440f99a
Gagan Hegde <gbhegde14@gmail.com>
2024-12-18 15:53:24 +0530

    chnage

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 const corsOptions = {
-    origin: 'https://your-render-domain.onrender.com', // Replace with Render domain
+    origin: 'https://gdg-citech-main.onrender.com', // Replace with Render domain
     methods: ['GET', 'POST'],
 };
 app.use(cors(corsOptions));
